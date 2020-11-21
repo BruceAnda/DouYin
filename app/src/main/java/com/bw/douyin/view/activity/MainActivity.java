e3de806437150dee8e7f65e157a12a11b1b058c5
BruceAnda <2668645098@qq.com>
2020-11-21 08:59:09 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/douyin/view/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.bw.douyin.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bw.douyin.R;
+
+/**
+ *  @author 赵亮
+ *  @date 2020/11/21 8:30
+ *  @desc 抖音主页面
+ *  @version 1.0
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/bw/douyin/view/activity/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.bw.douyin.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.bw.douyin.R;
+import com.bw.douyin.base.BaseActivity;
+
+/**
+ *  @author 赵亮
+ *  @date 2020/11/20 14:06
+ *  @desc App启动闪屏页面，展示软件logo
+ *  @version 1.0
+ */
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void init() {
+        setFullScreen();
+
+        // 延迟进入主页面
+        new CountDownTimer(1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                // 进入主页面
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }.start();
+    }
+}
